af9491b94547ac55a98b37f25de27ddd35fc0ea7
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2021-02-17 13:43:16 +0300

    Adding the logic for TomlNode and basic types: String, Boolean, Float, Int (#3)

--- src/ktomlMain/kotlin/com/akuleshov7/parsers/node/TomlNode.kt
+++ src/ktomlMain/kotlin/com/akuleshov7/parsers/node/TomlNode.kt
@@ -1,9 +1,13 @@
 package com.akuleshov7.parsers.node
 
+import platform.posix.exit
+import kotlin.system.exitProcess
+
 // Toml specification includes a list of supported data types: String, Integer, Float, Boolean, Datetime, Array, and Table.
 sealed class TomlNode(open val content: String) {
     open val children: MutableSet<TomlNode> = mutableSetOf()
     open var parent: TomlNode? = null
+    open fun printContent() = content
 
     fun insertBefore() {}
     fun insertAfter() {}
@@ -14,11 +18,37 @@ sealed class TomlNode(open val content: String) {
         children.add(child)
         child.parent = this
     }
+
+    companion object {
+        // number of spaces that is used to indent levels
+        const val INDENTING_LEVEL = 4
+
+        fun prettyPrint(node: TomlNode, level: Int = 0) {
+            val spaces = " ".repeat(INDENTING_LEVEL * level)
+            println("$spaces - ${node::class.simpleName} (${node.content})")
+            node.children.forEach { child ->
+                prettyPrint(child, level + 1)
+            }
+        }
+    }
 }
 
 class TomlFile : TomlNode("rootNode")
 
-class TomlSection(content: String) : TomlNode(content)
+class TomlSection(content: String) : TomlNode(content) {
+    var sectionName: String
+
+    init {
+        val sectionFromContent = "\\[(.*?)]"
+            .toRegex()
+            .find(content)
+            ?.groupValues
+            ?.get(1)
+
+        // FixMe: create parse exceptions
+        sectionName = sectionFromContent ?: throw Exception()
+    }
+}
 
 class TomlVariable(content: String) : TomlNode(content) {
     var key: TomlKey
@@ -26,14 +56,36 @@ class TomlVariable(content: String) : TomlNode(content) {
 
     init {
         val keyValue = content.split("=").map { it.trim() }
-        if (keyValue.size != 2) { throw Exception() }
+        // FixMe: need to throw a normal exception
+        if (keyValue.size != 2) {
+            throw Exception()
+        }
 
         key = TomlKey(keyValue[0])
-        value = TomlInt(keyValue[1])
+        value = parseValue(keyValue[1])
 
         this.appendChild(key)
         this.appendChild(value)
     }
+
+    private fun parseValue(contentStr: String): TomlValue =
+        if (contentStr == "true" || contentStr == "false") {
+            TomlBoolean(contentStr)
+        } else {
+            if (contentStr == "null") {
+                TomlNull()
+            } else {
+                try {
+                    TomlInt(contentStr)
+                } catch (e: NumberFormatException) {
+                    try {
+                        TomlFloat(contentStr)
+                    } catch (e: NumberFormatException) {
+                        TomlString(contentStr)
+                    }
+                }
+            }
+        }
 }
 
 class TomlKey(content: String) : TomlNode(content)
@@ -41,28 +93,17 @@ class TomlKey(content: String) : TomlNode(content)
 sealed class TomlValue(content: String) : TomlNode(content)
 
 class TomlString(content: String) : TomlValue(content)
-class TomlInt(content: String) : TomlValue(content)
-class TomlFloat(content: String) : TomlValue(content)
-class TomlDate(content: String) : TomlValue(content)
-class TomlBoolean(content: String) : TomlValue(content)
-class TomlTable(content: String): TomlValue(content)
 
-class TomlNull(content: String) : TomlValue(content)
-
-class TomlArray(content: String) : TomlValue(content) {
-    var values: List<String>
-    init {
-        values = listOf()
-    }
+class TomlInt(content: String) : TomlValue(content) {
+    var value: Int = content.toInt()
 }
 
-
-fun main() {
-    val parent = TomlFile()
-    val child1 = TomlVariable("a = 1234")
-    val child2 = TomlVariable("b = 12345")
-
-    parent.appendChild(child1)
-    parent.appendChild(child2)
+class TomlFloat(content: String) : TomlValue(content) {
+    var value: Float = content.toFloat()
 }
 
+class TomlBoolean(content: String) : TomlValue(content) {
+    var value: Boolean = content.toBoolean()
+}
+
+class TomlNull : TomlValue("null")
---
+++ src/ktomlTest/kotlin/node/parser/ValueParserTest.kt
@@ -0,0 +1,33 @@
+package node.parser
+
+import com.akuleshov7.parsers.node.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+enum class NodeType {
+    STRING, NULL, INT, FLOAT, BOOLEAN, INCORRECT
+}
+
+class ValueParserTest {
+    @Test
+    fun parsingTest() {
+        testTomlValue("a = gfhgfhfg", NodeType.STRING)
+        testTomlValue("a = 123456", NodeType.INT)
+        testTomlValue("a = 12.2345", NodeType.FLOAT)
+        testTomlValue("a = true", NodeType.BOOLEAN)
+        testTomlValue("a = false", NodeType.BOOLEAN)
+    }
+}
+
+fun getNodeType(v: TomlValue): NodeType = when (v) {
+    is TomlString -> NodeType.STRING
+    is TomlNull -> NodeType.NULL
+    is TomlInt -> NodeType.INT
+    is TomlFloat -> NodeType.FLOAT
+    is TomlBoolean -> NodeType.BOOLEAN
+    else -> NodeType.INCORRECT
+}
+
+fun testTomlValue(s: String, expectedType: NodeType) {
+    assertEquals(expectedType, getNodeType(TomlVariable(s).value))
+}
