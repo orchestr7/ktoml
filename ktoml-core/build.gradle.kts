ac7451b4584899a745b311a4d9e932990fc4913d
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2021-05-11 17:50:25 +0300

    Fixes for release process
    ### What's done:
    - changed workflow scripts
    - updated publishing configuration

--- ktoml-core/build.gradle.kts
+++ ktoml-core/build.gradle.kts
@@ -16,9 +16,15 @@ kotlin {
             }
         }
     }
-    linuxX64()
-    mingwX64()
-    macosX64()
+
+    val os = org.gradle.nativeplatform.platform.internal.DefaultNativePlatform.getCurrentOperatingSystem()
+    // Create a target for the host platform.
+    when {
+        os.isLinux -> linuxX64()
+        os.isWindows -> mingwX64()
+        os.isMacOsX -> macosX64()
+        else -> throw GradleException("Host OS '${os.name}' is not supported in Kotlin/Native $project.")
+    }
 
     sourceSets {
         val commonMain by getting {
