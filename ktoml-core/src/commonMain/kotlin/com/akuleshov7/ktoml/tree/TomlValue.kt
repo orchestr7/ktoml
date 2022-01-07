a4b78e99ba4fcbbdff694e3dfcca81d85ac2c4aa
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2022-01-06 20:31:08 -0700

    Refactor node constructors for easier encoding
    Classes deriving from TomlValue now expose their content property in an
    internal constructor, and parsing was moved to a secondary constructor.
    Since member functions are inaccessible before the super constructor is
    called, private functions used for parsing were moved to companion
    objects.

--- ktoml-core/src/commonMain/kotlin/com/akuleshov7/ktoml/tree/TomlValue.kt
+++ ktoml-core/src/commonMain/kotlin/com/akuleshov7/ktoml/tree/TomlValue.kt
@@ -23,105 +23,131 @@ public sealed class TomlValue(public val lineNo: Int) {
  * The only difference from the TOML specification (https://toml.io/en/v1.0.0) is that we will have one escaped symbol -
  * single quote and so it will be possible to use a single quote inside.
  */
-public class TomlLiteralString(
-    content: String,
-    lineNo: Int,
-    ktomlConf: KtomlConf = KtomlConf()) : TomlValue(lineNo) {
-    override var content: Any = if (content.startsWith("'") && content.endsWith("'")) {
-        val contentString = content.trimSingleQuotes()
-        if (ktomlConf.escapedQuotesInLiteralStringsAllowed) contentString.convertSingleQuotes() else contentString
-    } else {
-        throw TomlParsingException(
-            "Literal string should be wrapped with single quotes (''), it looks that you have forgotten" +
-                    " the single quote in the end of the following string: <$content>", lineNo
-        )
-    }
+public class TomlLiteralString
+internal constructor(
+    override var content: Any,
+    lineNo: Int
+) : TomlValue(lineNo) {
+    public constructor(
+        content: String,
+        lineNo: Int,
+        ktomlConf: KtomlConf = KtomlConf()
+    ) : this(content.verifyAndTrimQuotes(lineNo, ktomlConf), lineNo)
 
-    /**
-     * According to the TOML standard (https://toml.io/en/v1.0.0#string) single quote is prohibited.
-     * But in ktoml we don't see any reason why we cannot escape it. Anyway, by the TOML specification we should fail, so
-     * why not to try to handle this situation at least somehow.
-     *
-     * Conversion is done after we have trimmed technical quotes and won't break cases when the user simply used a backslash
-     * as the last symbol (single quote) will be removed.
-     */
-    private fun String.convertSingleQuotes(): String = this.replace("\\'", "'")
+    public companion object {
+        private fun String.verifyAndTrimQuotes(lineNo: Int, ktomlConf: KtomlConf): Any =
+            if (startsWith("'") && endsWith("'")) {
+                val contentString = trimSingleQuotes()
+                if (ktomlConf.escapedQuotesInLiteralStringsAllowed) contentString.convertSingleQuotes() else contentString
+            } else {
+                throw TomlParsingException(
+                    "Literal string should be wrapped with single quotes (''), it looks that you have forgotten" +
+                            " the single quote in the end of the following string: <$this>", lineNo
+                )
+            }
+
+        /**
+         * According to the TOML standard (https://toml.io/en/v1.0.0#string) single quote is prohibited.
+         * But in ktoml we don't see any reason why we cannot escape it. Anyway, by the TOML specification we should fail, so
+         * why not to try to handle this situation at least somehow.
+         *
+         * Conversion is done after we have trimmed technical quotes and won't break cases when the user simply used a backslash
+         * as the last symbol (single quote) will be removed.
+         */
+        private fun String.convertSingleQuotes(): String = this.replace("\\'", "'")
+    }
 }
 
 /**
  * Toml AST Node for a representation of string values: key = "value" (always should have quotes due to TOML standard)
  */
-public class TomlBasicString(content: String, lineNo: Int) : TomlValue(lineNo) {
-    override var content: Any = if (content.startsWith("\"") && content.endsWith("\"")) {
-        content.trimQuotes()
-            .checkOtherQuotesAreEscaped()
-            .convertSpecialCharacters()
-    } else {
-        throw TomlParsingException(
-            "According to the TOML specification string values (even Enums)" +
-                    " should be wrapped (start and end) with quotes (\"\"), but the following value was not: <$content>." +
-                    " Please note that multiline strings are not yet supported.",
-            lineNo
-        )
-    }
+public class TomlBasicString
+internal constructor(
+    override var content: Any,
+    lineNo: Int
+) : TomlValue(lineNo) {
+    public constructor(
+        content: String,
+        lineNo: Int
+    ) : this(content.verifyAndTrimQuotes(lineNo), lineNo)
 
-    private fun String.checkOtherQuotesAreEscaped(): String {
-        this.forEachIndexed { index, ch ->
-            if (ch == '\"' && (index == 0 || this[index - 1] != '\\')) {
+    public companion object {
+        private fun String.verifyAndTrimQuotes(lineNo: Int): Any =
+            if (startsWith("\"") && endsWith("\"")) {
+                trimQuotes()
+                    .checkOtherQuotesAreEscaped(lineNo)
+                    .convertSpecialCharacters(lineNo)
+            } else {
                 throw TomlParsingException(
-                    "Found invalid quote that is not escaped." +
-                            " Please remove the quote or use escaping" +
-                            " in <$this> at position = [$index].", lineNo
+                    "According to the TOML specification string values (even Enums)" +
+                            " should be wrapped (start and end) with quotes (\"\"), but the following value was not: <$this>." +
+                            " Please note that multiline strings are not yet supported.",
+                    lineNo
                 )
             }
-        }
-        return this
-    }
 
-    private fun String.convertSpecialCharacters(): String {
-        val resultString = StringBuilder()
-        var updatedOnPreviousStep = false
-        var i = 0
-        while (i < this.length) {
-            val newCharacter = if (this[i] == '\\' && i != this.length - 1) {
-                updatedOnPreviousStep = true
-                when (this[i + 1]) {
-                    // table that is used to convert escaped string literals to proper char symbols
-                    't' -> '\t'
-                    'b' -> '\b'
-                    'r' -> '\r'
-                    'n' -> '\n'
-                    '\\' -> '\\'
-                    '\'' -> '\''
-                    '"' -> '"'
-                    else -> throw TomlParsingException(
-                        "According to TOML documentation unknown" +
-                                " escape symbols are not allowed. Please check: [\\${this[i + 1]}]",
-                        lineNo
+        private fun String.checkOtherQuotesAreEscaped(lineNo: Int): String {
+            this.forEachIndexed { index, ch ->
+                if (ch == '\"' && (index == 0 || this[index - 1] != '\\')) {
+                    throw TomlParsingException(
+                        "Found invalid quote that is not escaped." +
+                                " Please remove the quote or use escaping" +
+                                " in <$this> at position = [$index].", lineNo
                     )
                 }
-            } else {
-                this[i]
             }
-            // need to skip the next character if we have processed special escaped symbol
-            if (updatedOnPreviousStep) {
-                updatedOnPreviousStep = false
-                i += 2
-            } else {
-                i += 1
-            }
-
-            resultString.append(newCharacter)
+            return this
         }
-        return resultString.toString()
+
+        private fun String.convertSpecialCharacters(lineNo: Int): String {
+            val resultString = StringBuilder()
+            var updatedOnPreviousStep = false
+            var i = 0
+            while (i < this.length) {
+                val newCharacter = if (this[i] == '\\' && i != this.length - 1) {
+                    updatedOnPreviousStep = true
+                    when (this[i + 1]) {
+                        // table that is used to convert escaped string literals to proper char symbols
+                        't' -> '\t'
+                        'b' -> '\b'
+                        'r' -> '\r'
+                        'n' -> '\n'
+                        '\\' -> '\\'
+                        '\'' -> '\''
+                        '"' -> '"'
+                        else -> throw TomlParsingException(
+                            "According to TOML documentation unknown" +
+                                    " escape symbols are not allowed. Please check: [\\${this[i + 1]}]",
+                            lineNo
+                        )
+                    }
+                } else {
+                    this[i]
+                }
+                // need to skip the next character if we have processed special escaped symbol
+                if (updatedOnPreviousStep) {
+                    updatedOnPreviousStep = false
+                    i += 2
+                } else {
+                    i += 1
+                }
+
+                resultString.append(newCharacter)
+            }
+            return resultString.toString()
+        }
     }
 }
 
 /**
  * Toml AST Node for a representation of Arbitrary 64-bit signed integers: key = 1
  */
-public class TomlLong(content: String, lineNo: Int) : TomlValue(lineNo) {
-    override var content: Any = content.toLong()
+public class TomlLong
+internal constructor(
+    override var content: Any,
+    lineNo: Int
+) : TomlValue(lineNo) {
+    public constructor(content: String, lineNo: Int) : this(content.toLong(), lineNo)
 }
 
 /**
@@ -129,15 +155,23 @@ public class TomlLong(content: String, lineNo: Int) : TomlValue(lineNo) {
  * Toml specification requires floating point numbers to be IEEE 754 binary64 values,
  * so it should be Kotlin Double (64 bits)
  */
-public class TomlDouble(content: String, lineNo: Int) : TomlValue(lineNo) {
-    override var content: Any = content.toDouble()
+public class TomlDouble
+internal constructor(
+    override var content: Any,
+    lineNo: Int
+) : TomlValue(lineNo) {
+    public constructor(content: String, lineNo: Int) : this(content.toDouble(), lineNo)
 }
 
 /**
  * Toml AST Node for a representation of boolean types: key = true | false
  */
-public class TomlBoolean(content: String, lineNo: Int) : TomlValue(lineNo) {
-    override var content: Any = content.toBoolean()
+public class TomlBoolean
+internal constructor(
+    override var content: Any,
+    lineNo: Int
+) : TomlValue(lineNo) {
+    public constructor(content: String, lineNo: Int) : this(content.toBoolean(), lineNo)
 }
 
 /**
@@ -151,16 +185,18 @@ public class TomlNull(lineNo: Int) : TomlValue(lineNo) {
 /**
  * Toml AST Node for a representation of arrays: key = [value1, value2, value3]
  */
-public class TomlArray(
+public class TomlArray
+internal constructor(
+    override var content: Any,
     private val rawContent: String,
-    lineNo: Int,
-    ktomlConf: KtomlConf = KtomlConf()
+    lineNo: Int
 ) : TomlValue(lineNo) {
-    override lateinit var content: Any
-
-    init {
+    public constructor(
+        rawContent: String,
+        lineNo: Int,
+        ktomlConf: KtomlConf = KtomlConf()
+    ) : this(rawContent.parse(lineNo, ktomlConf), rawContent, lineNo) {
         validateBrackets()
-        this.content = parse()
     }
 
     /**
@@ -169,54 +205,7 @@ public class TomlArray(
      * @param ktomlConf
      * @return converted array to a list
      */
-    public fun parse(ktomlConf: KtomlConf = KtomlConf()): List<Any> = rawContent.parse(ktomlConf)
-
-    /**
-     * recursively parse TOML array from the string: [ParsingArray -> Trimming values -> Parsing Nested Arrays]
-     */
-    private fun String.parse(ktomlConf: KtomlConf = KtomlConf()): List<Any> =
-            this.parseArray()
-                .map { it.trim() }
-                .map { if (it.startsWith("[")) it.parse(ktomlConf) else it.parseValue(lineNo, ktomlConf) }
-
-    /**
-     * method for splitting the string to the array: "[[a, b], [c], [d]]" to -> [a,b] [c] [d]
-     */
-    @Suppress("TOO_MANY_LINES_IN_LAMBDA")
-    private fun String.parseArray(): MutableList<String> {
-        // covering cases when the array is intentionally blank: myArray = []. It should be empty and not contain null
-        if (this.trimBrackets().isBlank()) {
-            return mutableListOf()
-        }
-
-        var numberOfOpenBrackets = 0
-        var numberOfClosedBrackets = 0
-        var bufferBetweenCommas = StringBuilder()
-        val result: MutableList<String> = mutableListOf()
-
-        this.trimBrackets().forEach {
-            when (it) {
-                '[' -> {
-                    numberOfOpenBrackets++
-                    bufferBetweenCommas.append(it)
-                }
-                ']' -> {
-                    numberOfClosedBrackets++
-                    bufferBetweenCommas.append(it)
-                }
-                // split only if we are on the highest level of brackets (all brackets are closed)
-                ',' -> if (numberOfClosedBrackets == numberOfOpenBrackets) {
-                    result.add(bufferBetweenCommas.toString())
-                    bufferBetweenCommas = StringBuilder()
-                } else {
-                    bufferBetweenCommas.append(it)
-                }
-                else -> bufferBetweenCommas.append(it)
-            }
-        }
-        result.add(bufferBetweenCommas.toString())
-        return result
-    }
+    public fun parse(ktomlConf: KtomlConf = KtomlConf()): List<Any> = rawContent.parse(lineNo, ktomlConf)
 
     /**
      * small validation for quotes: each quote should be closed in a key
@@ -227,6 +216,55 @@ public class TomlArray(
                 "Not able to parse the key: [$rawContent] as it does not have closing bracket",
                 lineNo
             )
+        }
+    }
+
+    public companion object {
+        /**
+         * recursively parse TOML array from the string: [ParsingArray -> Trimming values -> Parsing Nested Arrays]
+         */
+        private fun String.parse(lineNo: Int, ktomlConf: KtomlConf = KtomlConf()): List<Any> =
+            this.parseArray()
+                .map { it.trim() }
+                .map { if (it.startsWith("[")) it.parse(lineNo, ktomlConf) else it.parseValue(lineNo, ktomlConf) }
+
+        /**
+         * method for splitting the string to the array: "[[a, b], [c], [d]]" to -> [a,b] [c] [d]
+         */
+        @Suppress("TOO_MANY_LINES_IN_LAMBDA")
+        private fun String.parseArray(): MutableList<String> {
+            // covering cases when the array is intentionally blank: myArray = []. It should be empty and not contain null
+            if (this.trimBrackets().isBlank()) {
+                return mutableListOf()
+            }
+
+            var numberOfOpenBrackets = 0
+            var numberOfClosedBrackets = 0
+            var bufferBetweenCommas = StringBuilder()
+            val result: MutableList<String> = mutableListOf()
+
+            this.trimBrackets().forEach {
+                when (it) {
+                    '[' -> {
+                        numberOfOpenBrackets++
+                        bufferBetweenCommas.append(it)
+                    }
+                    ']' -> {
+                        numberOfClosedBrackets++
+                        bufferBetweenCommas.append(it)
+                    }
+                    // split only if we are on the highest level of brackets (all brackets are closed)
+                    ',' -> if (numberOfClosedBrackets == numberOfOpenBrackets) {
+                        result.add(bufferBetweenCommas.toString())
+                        bufferBetweenCommas = StringBuilder()
+                    } else {
+                        bufferBetweenCommas.append(it)
+                    }
+                    else -> bufferBetweenCommas.append(it)
+                }
+            }
+            result.add(bufferBetweenCommas.toString())
+            return result
         }
     }
 }
