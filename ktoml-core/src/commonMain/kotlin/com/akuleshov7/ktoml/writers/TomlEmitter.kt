0664aa072c7fe8b9d5ac89b0afddd3c6089f5b84
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2022-01-13 02:42:23 -0700

    Document TomlEmitter

--- ktoml-core/src/commonMain/kotlin/com/akuleshov7/ktoml/writers/TomlEmitter.kt
+++ ktoml-core/src/commonMain/kotlin/com/akuleshov7/ktoml/writers/TomlEmitter.kt
@@ -14,28 +14,78 @@ import kotlin.jvm.JvmStatic
 public abstract class TomlEmitter(ktomlConf: KtomlConf) {
     private val indentation = ktomlConf.indentation.value
 
+    /**
+     * The current indent depth, set by [indent] and [dedent].
+     */
     @Suppress("CUSTOM_GETTERS_SETTERS")
     public var indentDepth: Int = 0
         protected set
 
+    /**
+     * Increments [indentDepth], returning its value after incrementing.
+     *
+     * @return The new indent depth.
+     */
     public fun indent(): Int = ++indentDepth
+
+    /**
+     * Decrements [indentDepth], returning its value after decrementing.
+     *
+     * @return The new indent depth.
+     */
     public fun dedent(): Int = --indentDepth
 
+    /**
+     * Emits [fragment] as a raw [String] to the output.
+     *
+     * @param fragment The raw text to write to the output.
+     */
     protected abstract fun emit(fragment: String)
+
+    /**
+     * Emits [fragment] as a raw [Char] to the output.
+     *
+     * @param fragment The raw text to write to the output.
+     */
     protected abstract fun emit(fragment: Char)
 
+    /**
+     * Emits a newline character.
+     */
     public fun emitNewLine(): Unit = emit('\n')
 
+    /**
+     * Emits indentation up to the current [indentDepth].
+     */
     public fun emitIndent(): Unit = repeat(indentDepth) { emit(indentation) }
 
+    /**
+     * Emits [count] whitespace characters.
+     *
+     * @param count The number of whitespace characters to write.
+     */
     public fun emitWhitespace(count: Int = 1): Unit = repeat(count) { emit(' ') }
 
+    /**
+     * Emits a [comment], optionally making it end-of-line.
+     *
+     * @param comment
+     * @param endOfLine Whether the comment is at the end of a line, e.g. after a
+     * table header.
+     */
     public fun emitComment(comment: String, endOfLine: Boolean = false) {
         emit(if (endOfLine) " # " else "# ")
 
         emit(comment)
     }
 
+    /**
+     * Emits a [key]. Its type is inferred by its content, with bare keys being
+     * preferred. [emitBareKey] is called for simple keys, [emitQuotedKey] for
+     * non-simple keys.
+     *
+     * @param key
+     */
     public fun emitKey(key: String) {
         if (key matches bareKeyRegex) {
             emitBareKey(key)
@@ -44,21 +94,55 @@ public abstract class TomlEmitter(ktomlConf: KtomlConf) {
         }
     }
 
+    /**
+     * Emits a [key] as a bare key.
+     *
+     * @param key
+     */
     public fun emitBareKey(key: String): Unit = emit(key)
 
+    /**
+     * Emits a [key] as a quoted key, optionally making it literal (single-quotes).
+     *
+     * @param key
+     * @param isLiteral Whether the key should be emitted as a literal string
+     * (single-quotes).
+     */
     public fun emitQuotedKey(key: String, isLiteral: Boolean = false): Unit =
             emitValue(string = key, isLiteral)
 
+    /**
+     * Emits a key separator.
+     */
     public fun emitKeyDot(): Unit = emit('.')
 
+    /**
+     * Emits the table header start character.
+     */
     public fun startTableHeader(): Unit = emit('[')
 
+    /**
+     * Emits the table header end character.
+     */
     public fun endTableHeader(): Unit = emit(']')
 
+    /**
+     * Emits the table array header start characters.
+     */
     public fun emitTableArrayHeaderStart(): Unit = emit("[[")
 
+    /**
+     * Emits the table array header end characters.
+     */
     public fun emitTableArrayHeaderEnd(): Unit = emit("]]")
 
+    /**
+     * Emit a string value, optionally making it literal and/or multiline.
+     *
+     * @param string
+     * @param isLiteral Whether the string is literal (single-quotes).
+     * @param isMultiline Whether the string is multiline.
+     */
     public fun emitValue(
         string: String,
         isLiteral: Boolean = false,
@@ -78,6 +162,12 @@ public abstract class TomlEmitter(ktomlConf: KtomlConf) {
                 emit(quote)
             }
 
+    /**
+     * Emits an integer value, optionally changing its representation from decimal.
+     *
+     * @param integer
+     * @param representation How the integer will be represented in TOML.
+     */
     public fun emitValue(integer: Long, representation: IntegerRepresentation = DECIMAL): Unit =
             when (representation) {
                 DECIMAL -> emit(integer.toString())
@@ -96,6 +186,11 @@ public abstract class TomlEmitter(ktomlConf: KtomlConf) {
                 GROUPED -> TODO()
             }
 
+    /**
+     * Emits a floating-point value.
+     *
+     * @param float
+     */
     public fun emitValue(float: Double): Unit =
             emit(when (float) {
                 Double.NaN -> "nan"
@@ -104,18 +199,41 @@ public abstract class TomlEmitter(ktomlConf: KtomlConf) {
                 else -> float.toString()
             })
 
+    /**
+     * Emits a boolean value.
+     *
+     * @param boolean
+     */
     public fun emitValue(boolean: Boolean): Unit = emit(boolean.toString())
 
+    /**
+     * Emits the array start character.
+     */
     public fun startArray(): Unit = emit('[')
 
+    /**
+     * Emits the array end character.
+     */
     public fun endArray(): Unit = emit(']')
 
+    /**
+     * Emits the inline table start character.
+     */
     public fun startInlineTable(): Unit = emit('{')
 
+    /**
+     * Emits the inline table end character.
+     */
     public fun endInlineTable(): Unit = emit('}')
 
+    /**
+     * Emits an array/inline table element delimiter.
+     */
     public fun emitElementDelimiter(): Unit = emit(", ")
 
+    /**
+     * Emits a key-value delimiter.
+     */
     public fun emitPairDelimiter(): Unit = emit(" = ")
 
     public companion object {
