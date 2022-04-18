263ed2ed7c44166574b4dd0fc9ad714fa2677788
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2022-04-18 16:49:42 -0600

    Merge regex implementation and document

--- ktoml-core/src/macosX64Main/kotlin/com/akuleshov7/ktoml/utils/RegexesMac.kt
+++
@@ -1,9 +0,0 @@
-/**
- * Regexes used in tree writing.
- */
-
-package com.akuleshov7.ktoml.utils
-
-internal actual val controlCharacterRegex = commonControlCharacterRegex
-
-internal actual val unescapedBackslashRegex = commonUnescapedBackslashRegex
