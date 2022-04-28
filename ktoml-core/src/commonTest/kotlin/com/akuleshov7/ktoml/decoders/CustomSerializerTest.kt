4bb2a9f9206d8b06c603a7350645718a806ebc37
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-04-28 22:32:45 +0300

    Adding the support of custom serializers for simple cases

--- ktoml-core/src/commonTest/kotlin/com/akuleshov7/ktoml/decoders/CustomSerializerTest.kt
+++ ktoml-core/src/commonTest/kotlin/com/akuleshov7/ktoml/decoders/CustomSerializerTest.kt
@@ -1,44 +1,85 @@
 package com.akuleshov7.ktoml.decoders
 
-import kotlinx.serialization.decodeFromString
 import com.akuleshov7.ktoml.Toml
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlin.test.Ignore
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class CustomSerializerTest {
-    object ColorAsStringSerializer : KSerializer<Color> {
-        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Color", PrimitiveKind.STRING)
+    object SinglePropertyAsStringSerializer : KSerializer<SingleProperty> {
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("SingleProperty", PrimitiveKind.STRING)
 
-        override fun serialize(encoder: Encoder, value: Color) {
-            TODO()
+        override fun serialize(encoder: Encoder, value: SingleProperty) {
         }
 
-        override fun deserialize(decoder: Decoder): Color {
-            val value = decoder.decodeString()
-            return Color(value.toLong())
+        override fun deserialize(decoder: Decoder): SingleProperty {
+            val string = decoder.decodeString()
+            return SingleProperty(string.toLong() + 15)
         }
     }
 
-    @Serializable(with = ColorAsStringSerializer::class)
-    data class Color(val rgb: Long)
-
-    @Serializable
-    data class Settings(val background: Color, val foreground: Color)
+    @Serializable(with = SinglePropertyAsStringSerializer::class)
+    data class SingleProperty(val rgb: Long)
 
     @Test
+    fun testDecodingWithCustomSerializerSingleProperty() {
+        assertEquals(
+            SingleProperty(15),
+            Toml.decodeFromString(
+                """
+                    rgb = "0" 
+                """.trimIndent()
+            )
+        )
+    }
+
+    @Serializable(with = SeveralPropertiesAsStringSerializer::class)
+    data class SeveralProperties(val rgb: Long, val brg: Long)
+
+    object SeveralPropertiesAsStringSerializer : KSerializer<SeveralProperties> {
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Color", PrimitiveKind.STRING)
+
+        override fun serialize(encoder: Encoder, value: SeveralProperties) {
+        }
+
+        override fun deserialize(decoder: Decoder): SeveralProperties {
+            val string = decoder.decodeString()
+            return SeveralProperties(string.toLong() + 15, string.toLong())
+        }
+    }
+
+    @Test
+    @Ignore
+    fun testDecodingWithCustomSerializerSeveralProperties() {
+        assertEquals(
+            SeveralProperties(15, 1),
+            Toml.decodeFromString(
+                """
+                    rgb = "0" 
+                    brg = "1"
+                """.trimIndent()
+            )
+        )
+    }
+
+    @Serializable
+    data class Settings(val background: SingleProperty, val foreground: SingleProperty)
+
+    @Test
+    @Ignore
     fun testDecodingWithCustomSerializer() {
         println(Toml.decodeFromString<Settings>(
             """
                 [background]
-                    rgb = 0
+                    rgb = "0"
                 [foreground]
-                    rgb = 0
+                    rgb = "0"
             """.trimIndent()
         ))
     }
---
+++ ktoml-core/src/commonTest/kotlin/com/akuleshov7/ktoml/decoders/SurrogateSerializerTest.kt
@@ -0,0 +1,48 @@
+package com.akuleshov7.ktoml.decoders
+
+import com.akuleshov7.ktoml.Toml
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+class SurrogateTest {
+    object ColorSerializer : KSerializer<Color> {
+        override val descriptor: SerialDescriptor = ColorSurrogate.serializer().descriptor
+
+        override fun serialize(encoder: Encoder, value: Color) {
+            val surrogate = ColorSurrogate((value.rgb shr 16) and 0xff, (value.rgb shr 8) and 0xff, value.rgb and 0xff)
+            encoder.encodeSerializableValue(ColorSurrogate.serializer(), surrogate)
+        }
+
+        override fun deserialize(decoder: Decoder): Color {
+            val surrogate = decoder.decodeSerializableValue(ColorSurrogate.serializer())
+            return Color((surrogate.r shl 16) or (surrogate.g shl 8) or surrogate.b)
+        }
+    }
+
+    @Serializable
+    @SerialName("Color")
+    private class ColorSurrogate(val r: Int, val g: Int, val b: Int) {
+        init {
+            require(r in 0..255 && g in 0..255 && b in 0..255)
+        }
+    }
+
+    @Serializable(with = ColorSerializer::class)
+    class Color(val rgb: Int)
+
+    @Test
+    @Ignore
+    fun testDecodingWithCustomSerializer() {
+        println(Toml.decodeFromString<Color>(
+            """
+                r = 5
+                g = 6
+                b = 7
+            """.trimIndent()
+        ))
+    }
+}
