56dcb1988068b9d4ae178f737532e33473e29594
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2022-05-05 00:10:47 -0600

    Add serialization annotations

---
+++ ktoml-core/src/commonMain/kotlin/com/akuleshov7/ktoml/annotations/TomlComments.kt
@@ -0,0 +1,46 @@
+package com.akuleshov7.ktoml.annotations
+
+import kotlin.annotation.AnnotationTarget.PROPERTY
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialInfo
+
+/**
+ * Specifies comments to be applied the TOML element produced by a property during
+ * serialization. Has no effect on deserialization.
+ *
+ * ```kotlin
+ * @Serializable
+ * data class Data(
+ *     @TomlComments(inline = "Inline")
+ *     val x: Int,
+ *     @TomlComments(
+ *         "Descriptive comment 1",
+ *         "Descriptive comment 2"
+ *     )
+ *     val y: Int
+ * )
+ *
+ * val data = Data(x = 3, y = 7)
+ * ```
+ *
+ * would produce:
+ *
+ * ```toml
+ * x = 3 # Inline
+ *
+ * # Descriptive comment 1
+ * # Descriptive comment 2
+ * y = 7
+ * ```
+ *
+ * @property lines Comment lines to be placed before the TOML element.
+ * @property inline A comment placed *inline* with the TOML element, at the end
+ * of the line. If empty (the default), no comment will be written.
+ */
+@OptIn(ExperimentalSerializationApi::class)
+@SerialInfo
+@Target(PROPERTY)
+public annotation class TomlComments(
+    vararg val lines: String,
+    val inline: String = ""
+)
---
+++ ktoml-core/src/commonMain/kotlin/com/akuleshov7/ktoml/annotations/TomlLiteral.kt
@@ -0,0 +1,35 @@
+package com.akuleshov7.ktoml.annotations
+
+import kotlin.annotation.AnnotationTarget.PROPERTY
+import kotlin.annotation.AnnotationTarget.TYPE_PARAMETER
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialInfo
+
+/**
+ * Marks a TOML string element as literal. Has no effect on deserialization.
+ *
+ * ```kotlin
+ * @Serializable
+ * data class Data(
+ *     @TomlLiteral
+ *     val text: String,
+ *     val list: List<@TomlLiteral String>
+ * )
+ *
+ * val data = Data(
+ *     text = "\"literal\"",
+ *     list = listOf("\\[a-z]*\\")
+ * )
+ * ```
+ *
+ * would produce:
+ *
+ * ```toml
+ * text = '"literal"'
+ * list = [ '\[a-z]*\' ]
+ * ```
+ */
+@OptIn(ExperimentalSerializationApi::class)
+@SerialInfo
+@Target(PROPERTY, TYPE_PARAMETER)
+public annotation class TomlLiteral
