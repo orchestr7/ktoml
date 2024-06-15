c7fb88133557965a190fccd2fcb3116d1e871533
akuleshov7 <andrewkuleshov7@gmail.com>
2024-06-15 17:54:34 +0300

    Updating kotlinx.serialization to 1.7.0

--- buildSrc/src/main/kotlin/com/akuleshov7/Versions.kt
+++ buildSrc/src/main/kotlin/com/akuleshov7/Versions.kt
@@ -8,5 +8,5 @@ object Versions {
     const val KOTLIN = "2.0.0"
     const val JUNIT = "5.7.1"
     const val OKIO = "3.9.0"
-    const val SERIALIZATION = "1.5.0"
+    const val SERIALIZATION = "1.7.0"
 }
