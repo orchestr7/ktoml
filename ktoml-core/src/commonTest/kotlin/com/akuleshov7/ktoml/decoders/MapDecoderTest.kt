f632a4ce32093eabd72cef4038533405149773c7
Evgenii <bykov.evgeniy21@gmail.com>
2025-04-21 01:29:57 +0200

    Support map of objects decoding (#304)
    * Support map of objects decoding

--- ktoml-core/src/commonTest/kotlin/com/akuleshov7/ktoml/decoders/MapDecoderTest.kt
+++ ktoml-core/src/commonTest/kotlin/com/akuleshov7/ktoml/decoders/MapDecoderTest.kt
@@ -3,14 +3,16 @@ package com.akuleshov7.ktoml.decoders
 import com.akuleshov7.ktoml.Toml
 import com.akuleshov7.ktoml.exceptions.MissingRequiredPropertyException
 import com.akuleshov7.ktoml.exceptions.TomlDecodingException
-import com.akuleshov7.ktoml.exceptions.UnsupportedDecoderException
-
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.Serializable
-import kotlin.test.*
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
-class PlainMapDecoderTest {
+class MapDecoderTest {
     @Serializable
     private data class TestDataMap(
         val text: String = "Test",
@@ -145,6 +147,113 @@ class PlainMapDecoderTest {
         assertEquals(
             data,
             decoded
+        )
+    }
+
+    @Test
+    fun decodeMapOfObjects() {
+        @Serializable
+        data class Coin(
+            val scale: Long,
+            @SerialName("default_volume")
+            val defaultVolume: Double,
+            @SerialName("skip_in_orderbook")
+            val skipInOrderbook: Double,
+        )
+        @Serializable
+        data class CoinsConf(
+            val coins: Map<String, Coin>
+        )
+
+        //language=toml
+        val toml = """
+            [coins.bitcoin]
+            scale = 8
+            default_volume = 0.1
+            skip_in_orderbook = 0.00001
+
+            [coins.ethereum]
+            scale = 8
+            default_volume = 0.2
+            skip_in_orderbook = 0.00001
+
+            [coins.tether]
+            scale = 2
+            default_volume = 100.0
+            skip_in_orderbook = 0.0001
+        """.trimIndent()
+
+        assertEquals(
+            CoinsConf(
+                mapOf(
+                    "bitcoin" to Coin(8, 0.1, 0.00001),
+                    "ethereum" to Coin(8, 0.2, 0.00001),
+                    "tether" to Coin(2, 100.0, 0.0001)
+                )
+            ),
+            Toml().decodeFromString(CoinsConf.serializer(), toml),
+        )
+    }
+
+    @Test
+    fun decodeMapWithQuotedNames() {
+        @Serializable
+        data class Animal(
+            val name: String,
+            val vocal: String
+        )
+        @Serializable
+        data class Animals(
+            val animals: Map<String, Animal>
+        )
+
+        //language=toml
+        val tomlString = """
+            [animals."my cat"]
+            name = "maunz"
+            vocal = "miau"
+            
+            [animals."my dog"]
+            name = "bello"
+            vocal = "wuff"
+        """.trimIndent()
+        assertEquals(
+            Animals(
+                mapOf(
+                    "my cat" to Animal("maunz", "miau"),
+                    "my dog" to Animal("bello", "wuff")
+                )
+            ),
+            Toml.decodeFromString<Animals>(tomlString),
+        )
+    }
+
+    @Test
+    fun decodeMapWithInlineTable() {
+        @Serializable
+        data class Table(
+            val item1: String,
+            val item2: Map<String, String>,
+        )
+        @Serializable
+        data class Wrapper(
+            val table: Table,
+        )
+
+        //language=toml
+        val toml = """
+            [table]
+            "item1" = "val1"
+            "item2" = { key1 = "val2", key2 = "val3" }
+        """.trimIndent()
+        assertEquals(
+            Wrapper(
+                Table(
+                    item1 = "val1",
+                    item2 = mapOf("key1" to "val2", "key2" to "val3")
+                )
+            ),
+            Toml.decodeFromString<Wrapper>(toml),
         )
     }
 }
