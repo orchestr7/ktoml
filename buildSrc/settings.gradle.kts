d8767e6190664dce9afe0ad046d1d0ab69b6a95a
White Rabbit <bonycatbony@gmail.com>
2026-05-25 16:38:01 +0300

    Update settings.gradle.kts

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,9 +1,1 @@
-pluginManagement {
-    repositories {
-        // Kotlin 2.2.x artifacts are available on repo1.maven.org but not yet synced to
-        // repo.maven.apache.org (which mavenCentral() uses). Once synced, this can be
-        // replaced with mavenCentral().
-        maven { url = uri("https://repo1.maven.org/maven2/") }
-        gradlePluginPortal()
-    }
-}
+
